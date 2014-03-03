f553a05d3e8cae73e20af66d4ff1d79c6ea2f716
Mark Dewey <mdewey@problemsolutions.net>
2014-03-02 22:53:39 -0500

    added some stuff

---
+++ C#/FizzBang/FizzBang/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBang
+{
+    class Program
+    {
+
+        public class OutCondition
+        {
+            public int number { get; set; }
+            public string output { get; set; }
+
+            public OutCondition(int n, string o)
+            {
+                this.number = n;
+                this.output = o;
+            }
+        }
+
+        public class FizzBanging
+        {
+            public IEnumerable<OutCondition> OutConditions { get; set; }
+
+            public FizzBanging(IEnumerable<OutCondition> outConditions)
+            {
+                this.OutConditions = outConditions;
+            }
+
+            public void DoTheFizzBanging(int start = 0, int end = 100)
+            {
+
+                this.CheckFizzBanging(start);
+                if (start != end)
+                {
+                    this.DoTheFizzBanging(start + 1);
+                }
+                
+            }
+
+            public void CheckFizzBanging(int i)
+            {
+                var doPrint = true;
+
+                foreach (var condition in OutConditions.Where(condition => i %  condition.number == 0))
+                {
+                    Console.Write(condition.output);
+                    doPrint = false;
+                }
+                if (doPrint)
+                    Console.Write(i);
+                Console.WriteLine();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var con = new List<OutCondition>
+            {
+                new OutCondition(3, "Fizz"),
+                new OutCondition(5, "Bang")
+            };
+
+            new FizzBanging(con).DoTheFizzBanging();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/LinqTree/LinqTree/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTree
+{
+    class Program
+    {
+        public static class TreeToEnumerableEx
+        {
+            public static IEnumerable<T> AsDepthFirstEnumerable<T>(this T head, Func<T, IEnumerable<T>> childrenFunc)
+            {
+                yield return head;
+                foreach (var node in childrenFunc(head))
+                {
+                    foreach (var child in AsDepthFirstEnumerable(node, childrenFunc))
+                    {
+                        yield return child;
+                    }
+                }
+            }
+
+            public static IEnumerable<T> AsBreadthFirstEnumerable<T>(this T head, Func<T, IEnumerable<T>> childrenFunc)
+            {
+                yield return head;
+                var last = head;
+                foreach (var node in AsBreadthFirstEnumerable(head, childrenFunc))
+                {
+                    foreach (var child in childrenFunc(node))
+                    {
+                        yield return child;
+                        last = child;
+                    }
+                    if (last.Equals(node)) yield break;
+                }
+            }
+        }
+        public class Node
+        {
+            private readonly List<Node> children = new List<Node>();
+
+            public Node(int id)
+            {
+                Id = id;
+            }
+
+            public IEnumerable<Node> Children { get { return children; } }
+
+            public Node AddChild(int id)
+            {
+                var child = new Node(id);
+                children.Add(child);
+                return child;
+            }
+
+            public int Id { get; private set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            // build the tree in breadth-first order
+            var id = 1;
+            var depthFirst  = new Node(id);
+            var bf2 = depthFirst.AddChild(++id);
+            var bf3 = depthFirst.AddChild(++id);
+            var bf4 = bf2.AddChild(++id);
+            var bf5 = bf2.AddChild(++id);
+            var bf6 = bf3.AddChild(++id);
+            var bf7 = bf3.AddChild(++id);
+
+           // find all nodes in depth-first order and select just the Id of each node  
+            var IDs = from node in depthFirst.AsDepthFirstEnumerable(x => x.Children)
+                      select node.Id;
+
+        }
+    }
+}
