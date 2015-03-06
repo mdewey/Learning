123442da3d6ed9a72a8dedd37b3b2392b23f2062
Mark Dewey <mark.t.dewey@gmail.com>
2015-03-06 08:37:02 -0500

    one liner hackerrank solution

---
+++ C#/GameOfThrones-1/GameOfThrones-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfThrones_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passord = Console.ReadLine() ?? "";
+
+            var output = (passord.Distinct().Select(s => new { count = passord.Count(a => a == s), letter = s }).Count(w => w.count % 2 == 1) <= 1) ? "YES" : "NO";
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}
